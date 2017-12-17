adda10a7867631adca67da47a89c4e6f7d3ec6dd
Ryan Forgie <ryan.j.forgie@gmail.com>
2017-12-17 16:43:26 +0000

    Begin basic models

---
+++ EloTableTennis/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EloTableTennis.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+
+        public ICollection<PlayerGame> PlayerGames { get; set; }
+    }
+}
